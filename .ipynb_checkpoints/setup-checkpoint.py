a904a29e4f6940b9228642648549addcaac58666
haibuihoang <haibuihoang@gmail.com>
2023-05-26 15:55:24 +0200

    Clean build

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-package = Extension('backend', ['backend.pyx'], include_dirs=[numpy.get_include()])
-setup(ext_modules=cythonize([package]))
